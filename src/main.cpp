7915125b927d05f356416e52b2282e388677a3a0
Sweet Acid <antoniu.ilea@gmail.com>
2022-03-04 16:04:30 +0200

    Instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 typedef uint32_t u32;
 
@@ -13,16 +15,51 @@ const u32 HEIGHT = 600;
 class HelloTriangleApplication {
 public:
   void run() {
-    initVulkan();
     initWindow();
+    initVulkan();
     mainLoop();
     cleanup();
   }
 
 private:
   GLFWwindow *window;
+  VkInstance instance;
 
-  void initVulkan() {}
+  void createInstance() {
+    VkApplicationInfo app{};
+    app.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app.pApplicationName = "Hello Triangle";
+    app.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    app.pEngineName = "None";
+    app.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+    app.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &app;
+
+    u32 glfwExtensionsCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionsCount);
+    createInfo.enabledExtensionCount = glfwExtensionsCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+      throw std::runtime_error("failed to create instance");
+
+    u32 extensionCount = 0;
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                           extensions.data());
+
+    std::cout << "available extensions:\n";
+    for (const auto &extension : extensions)
+      std::cout << '\t' << extension.extensionName << '\n';
+  }
+
+  void initVulkan() { createInstance(); }
 
   void initWindow() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -37,6 +74,7 @@ private:
   }
 
   void cleanup() {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
   }
